2a768b28de5c042e50184bf327df700409174977
Jianpan <497231226@qq.com>
2017-12-26 11:07:30 +0800

    在Scala中，可以通过定义类似getter和setter的方法，分别叫做value和value_=

--- wordcount/src/main/java/test.scala
+++ wordcount/src/main/java/test.scala
@@ -1,17 +1,32 @@
-object test {
-  def main (args: Array[String]): Unit ={
-    val myCounter = new Counter
-    myCounter.increment(myCounter.value) //或者也可以不用圆括号，写成myCounter.increment
-    println(myCounter.current)
-    myCounter.value = 3
-    myCounter.increment(1)
-    println(myCounter.value)
-  }
-}
+
 
 class Counter {
-  var value = 0
-  def increment(step:Int): Unit = value += step //去掉了大括号
-  def current(): Int = {value}  //作为对比，这里依然保留大括号
+  //value变成私有字段以后，Scala又没有提供getter和setter方法，怎么可以访问value字段呢？
+  // 解决方案是，在Scala中，可以通过定义类似getter和setter的方法，分别叫做value和value_=
+
+  private var privateValue = 0
+
+  def value = privateValue;
+
+  def value_=(newValue: Int) {
+    if (newValue > 0) privateValue = newValue
+  }
+
+  def increment(step: Int): Unit = value += step //去掉了大括号
+  def current(): Int = {
+    value
+  } //作为对比，这里依然保留大括号
+}
+
+
+object test {
+  def main(args: Array[String]): Unit = {
+    val myCounter = new Counter
+    println(myCounter.value)
+    myCounter.value = 3
+    println(myCounter.value)
+    myCounter.increment(1)
+    println(myCounter.current)
+  }
 }
 
