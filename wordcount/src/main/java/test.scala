ac2a76d8d6e76b4d20692316f54f036758244287
Jianpan <497231226@qq.com>
2017-12-26 10:51:57 +0800

    getter和setter方法

--- wordcount/src/main/java/test.scala
+++ wordcount/src/main/java/test.scala
@@ -1,14 +1,17 @@
 object test {
   def main (args: Array[String]): Unit ={
     val myCounter = new Counter
-    myCounter.increment() //或者也可以不用圆括号，写成myCounter.increment
+    myCounter.increment(myCounter.value) //或者也可以不用圆括号，写成myCounter.increment
     println(myCounter.current)
+    myCounter.value = 3
+    myCounter.increment(1)
+    println(myCounter.value)
   }
 }
 
 class Counter {
-  private var value = 0
-  def increment(): Unit = value += 1 //去掉了大括号
+  var value = 0
+  def increment(step:Int): Unit = value += step //去掉了大括号
   def current(): Int = {value}  //作为对比，这里依然保留大括号
 }
 
